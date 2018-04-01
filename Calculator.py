24ad545f89934994d1b71595f0ef63e5d7c6ed96
bradszalach <37637591+bradszalach@users.noreply.github.com>
2018-04-01 16:31:54 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,34 @@
+#Function add num1 and num2
+def add(num1, num2):
+	return num1 + num2
+
+#Function subtracts num1 and num2
+def sub(num1, num2):
+	return num1 - num2
+
+#Function multiplies num1 and num2
+def mul(num1, num2):
+	return num1 * num2
+
+#Function divides num1 and num2
+def div(num1, num2):
+	return num1 / num2
+	
+def main():
+	operation = input("What do you want to do? +,-,*,/ :")
+	if(operation != '+' and operation != '-' and operation != '*' and operation != '/'):
+		print("Invalid option")
+	else:
+		num1 = int(input("Enter First Number: "))
+		num2 = int(input("Enter Second Number: "))
+	if(operation == '+'):
+		print(add(num1, num2))
+	elif(operation == '-'):
+		print(sub(num1, num2))
+	elif(operation == '*'):
+		print(mul(num1, num2))
+	elif(operation == '/'):
+		print(div(num1, num2))
+		
+		
+main()		
